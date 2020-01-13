1a054ffdcc2380853d1bf6cf2566445ff65f901f
heitorlessa <lessa@amazon.co.uk>
2020-01-12 19:13:40 -0600

    feat(amplify): add graphql example

---
+++ graphql-amplify-cli/src/graphql/subscriptions.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = `subscription OnCreateTodo($owner: String) {
+  onCreateTodo(owner: $owner) {
+    id
+    name
+    description
+    owner
+  }
+}
+`;
+export const onUpdateTodo = `subscription OnUpdateTodo($owner: String) {
+  onUpdateTodo(owner: $owner) {
+    id
+    name
+    description
+    owner
+  }
+}
+`;
+export const onDeleteTodo = `subscription OnDeleteTodo($owner: String) {
+  onDeleteTodo(owner: $owner) {
+    id
+    name
+    description
+    owner
+  }
+}
+`;
